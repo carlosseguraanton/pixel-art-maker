99e82d195759dabe6222b4ecadd5ba09c00f56bd
Carlos Segura Antón <peleino_pilas@hotmail.com>
2020-02-13 18:02:33 +0100

    Add files via upload

--- designs.js
+++ designs.js
@@ -13,7 +13,7 @@ const displayHeight = document.getElementById('gridHeightDisplay');
 const displayWidth = document.getElementById('gridWidthDisplay');
 const userHeight = document.getElementById('inputHeight');
 const userWidth = document.getElementById('inputWidth');
-    // let grid = $('#pixelCanvas');
+// let grid = $('#pixelCanvas');
 const grid = document.getElementById('pixelCanvas');
 const gridCanvas = document.getElementById('gridCanvas');
 let gridTileMode = PAINT // controls paint or erase of grid cells (td's)
@@ -31,17 +31,17 @@ theGridSize.submitGrid.onclick = function makeGrid(event) {
 
     // grid.children().remove(); // delete any previous table rows
     while (grid.hasChildNodes()) {
-      grid.removeChild(grid.lastChild); // delete any previous table rows
+        grid.removeChild(grid.lastChild); // delete any previous table rows
     }
 
-//Build the grid row by row and then append to the table
-//  project rubrics requires use of for and while loops
+    //Build the grid row by row and then append to the table
+    //  project rubrics requires use of for and while loops
 
     let tableRows = '';
     let r = 1;
     while (r <= rows) {
         tableRows += '<tr>';
-        for (let c=1; c <= columns; c++) {
+        for (let c = 1; c <= columns; c++) {
             tableRows += '<td></td>';
         }
         tableRows += '</tr>';
@@ -54,36 +54,36 @@ theGridSize.submitGrid.onclick = function makeGrid(event) {
     grid.classList.toggle('flyItIn2'); // use two to trigger reflow
 
     // grid.style.animationPlayState="paused";
-// Listen for click to paint or erase a tile
+    // Listen for click to paint or erase a tile
     // grid.on('click', 'td', function() {
     //     paintEraseTiles($(this));
     // });
-    grid.addEventListener("click", function(event) {
+    grid.addEventListener("click", function (event) {
         event.preventDefault();
         paintEraseTiles(event.target);
     });
 
-// Listen for mouse down, up and over for continuous paint and erase
+    // Listen for mouse down, up and over for continuous paint and erase
 
     // grid.on('mousedown', function(event) {
-    grid.addEventListener('mousedown', function(event) {
+    grid.addEventListener('mousedown', function (event) {
         event.preventDefault();
         mouseIsDown = event.which === 1 ? true : false;
     });
 
     // document.on('mouseup', function() {
-    document.addEventListener('mouseup', function(event) {
+    document.addEventListener('mouseup', function (event) {
         event.preventDefault();
         mouseIsDown = false;
     });
 
     // grid.on('mouseover', 'td', function() {
-    grid.addEventListener('mouseover', function(event) {
+    grid.addEventListener('mouseover', function (event) {
         // if (mouseIsDown) {paintEraseTiles($(this));}
         event.preventDefault();
-        if (mouseIsDown) {paintEraseTiles(event.target);}
+        if (mouseIsDown) { paintEraseTiles(event.target); }
     }); // end continuous paint and erase
-// }); // end grid
+    // }); // end grid
 }; // end grid
 
 // paint or erase cells based on the mode (girdTileMode)
@@ -99,42 +99,42 @@ function paintEraseTiles(targetCell) {
 }
 
 // Display how many cells high the grid will be
-    // $('#inputHeight').on('input', function() {
-        // $('#gridHeightDisplay').text(' ' + $(this).val());
-theGridSize.height.oninput = function (){
+// $('#inputHeight').on('input', function() {
+// $('#gridHeightDisplay').text(' ' + $(this).val());
+theGridSize.height.oninput = function () {
     displayHeight.innerHTML = ' ' + theGridSize.height.value;
-// });
+    // });
 };
 // Display how many cells wide the grid will be
-    // $('#inputWidth').on('input', function() {
-        // $('#gridWidthDisplay').text(' ' + $(this).val());
-theGridSize.width.oninput = function (){
+// $('#inputWidth').on('input', function() {
+// $('#gridWidthDisplay').text(' ' + $(this).val());
+theGridSize.width.oninput = function () {
     displayWidth.innerHTML = ' ' + theGridSize.width.value;
     // });
 };
 
 // $('#colorPicker').on('input', function() {
 // $('.paintOrErase').text(' ' + gridTileMode);
-userColor.oninput = function (){
+userColor.oninput = function () {
     gridTileMode = PAINT;
     tileMode.innerHTML = ' ' + gridTileMode;
 };
 // Erase colors from the grid
 
 // clear.on('click', function(){
-document.getElementById('clearGrid').addEventListener('click', function() {
+document.getElementById('clearGrid').addEventListener('click', function () {
     gridCanvas.classList.toggle('rotateCanvas'); // rotate the Design Canvas div
     let tiles = grid.getElementsByTagName('td');
     // grid.children().children().removeAttr("style");
-    for(let i = 0; i <= tiles.length; i++) {
+    for (let i = 0; i <= tiles.length; i++) {
         tiles[i].style.backgroundColor = 'transparent';
     }
 });
 
 // set the mode to PAINT or ERASE
 // $('button').on('click', function(event) {
-document.getElementById('mode').addEventListener('click', function(event) {
-    gridTileMode = event.target.className.indexOf('paint') >=0 ? PAINT : ERASE;
+document.getElementById('mode').addEventListener('click', function (event) {
+    gridTileMode = event.target.className.indexOf('paint') >= 0 ? PAINT : ERASE;
     // $('.paintOrErase').text(' ' + gridTileMode);
     tileMode.innerHTML = ' ' + gridTileMode;
 }); // end mode change / display
